4abb0e788deeab75a19b773409e4a3fc18d63707
yogoken <yogo.k@husky.neu.edu>
2016-07-27 10:03:30 +0900

    Update strong params for devise 4 and registration/new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).push(:name, :member, :profile, :works)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :member, :profile, :works])
   end
 
 end
