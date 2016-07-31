943fda9605badcf6d9563c01d7448620abb0004f
yogoken <yogo.k@husky.neu.edu>
2016-07-31 21:35:23 +0900

    Change user_params into update_params in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update(user_params)
+    if @user.update(update_params)
       redirect_to root_url, notice: "Edited profile successfully"
     else
       redirect_to edit_user_path, alert: 'All forms can\'t be blank'
@@ -21,7 +21,14 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
-    def user_params
-      params.require(:user).permit(:username, :email, :password, :member, :profile, :works)
+    def update_params
+      params.require(:user).permit(
+        :username,
+        :email,
+        :password,
+        :member,
+        :profile,
+        :works
+        )
     end
 end
