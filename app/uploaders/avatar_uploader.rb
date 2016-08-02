c9eaf28d87660172fd5d17c7f4f749163934814a
yogoken <yogo.k@husky.neu.edu>
2016-08-02 12:21:46 +0900

    Generate uploaders/avatar

---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,17 @@
+class AvatarUploader < CarrierWave::Uploader::Base
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def default_url
+    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+end
