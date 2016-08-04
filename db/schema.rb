313dccc5a7e570400b88503704a1d4797a3bf24a
yogoken <yogo.k@husky.neu.edu>
2016-08-05 08:01:50 +0900

    Add likes_count to prototypes table and display like in show view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804223957) do
+ActiveRecord::Schema.define(version: 20160804224903) do
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id",      limit: 4
@@ -34,12 +34,13 @@ ActiveRecord::Schema.define(version: 20160804223957) do
   add_index "prototype_images", ["prototype_id"], name: "index_prototype_images_on_prototype_id", using: :btree
 
   create_table "prototypes", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4
-    t.string   "title",      limit: 255
-    t.string   "catch_copy", limit: 255
-    t.text     "concept",    limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "user_id",     limit: 4
+    t.string   "title",       limit: 255
+    t.string   "catch_copy",  limit: 255
+    t.text     "concept",     limit: 65535
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+    t.integer  "likes_count", limit: 4,     default: 0
   end
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
