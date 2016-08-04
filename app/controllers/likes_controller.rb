8c8a2c406814e712c6389bb11e5838bd849553c9
yogoken <yogo.k@husky.neu.edu>
2016-08-05 08:27:13 +0900

    Add create action and view file to make logic

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,18 @@
 class LikesController < ApplicationController
+
+  before_action :set_prototype, only: [:create, :destroy]
+  before_action :set_likes,     only: [:create, :destroy]
+
+  def create
+    @like = @prototype.likes.create(user_id: current_user.id)
+  end
+
+  private
+    def set_prototype
+      @prototype = Prototype.find(params[:prototype_id])
+    end
+
+    def set_likes
+      @likes = @prototype.likes
+    end
 end
