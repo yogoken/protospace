b762de4090b9ab5cbd37070c7d9efab300a8b91e
yogoken <yogo.k@husky.neu.edu>
2016-08-06 21:56:04 +0900

    Add create action on comments controller to display each comment

--- app/controllers/prototypes/comments_controller.rb
+++ app/controllers/prototypes/comments_controller.rb
@@ -1,3 +1,21 @@
 class Prototypes::CommentsController < ApplicationController
-  
+  before_action :set_prototype, only: [:create]
+  before_action :set_comments,  only: [:create]
+
+  def create
+    @comment = @prototype.comments.create(comment_params)
+  end
+
+  private
+    def set_prototype
+      @prototype = Prototype.find(params[:prototype_id])
+    end
+
+    def set_comments
+      @comments = @prototype.comments.includes(:user, :prototype)
+    end
+
+    def comment_params
+      params.require(:comment).permit(:user_id, :prototype_id, :content)
+    end
 end
