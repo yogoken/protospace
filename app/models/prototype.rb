e02ea5595a4b254878f88bc98a4ed726cd2261dd
yogoken <yogo.k@husky.neu.edu>
2016-08-06 20:56:47 +0900

    Add routing/associations and create comments table

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,6 +2,7 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many   :prototype_images, dependent: :destroy
   has_many   :likes,            dependent: :destroy
+  has_many   :comments,         dependent: :destroy
 
   accepts_nested_attributes_for :prototype_images, allow_destroy: true,
                                                    reject_if: :all_blank
