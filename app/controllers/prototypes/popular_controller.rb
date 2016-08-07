dc3f15608b5febddf0340a3bb8933148fd1e23c0
yogoken <yogo.k@husky.neu.edu>
2016-08-07 15:51:59 +0900

    Minor error changes from like_count to likes_count on popular controller

--- app/controllers/prototypes/popular_controller.rb
+++ app/controllers/prototypes/popular_controller.rb
@@ -1,6 +1,7 @@
 class Prototypes::PopularController < ApplicationController
   def index
-    @prototypes = Prototype.order("prototypes.like_count DESC").eager_load(:user, :prototype_images)
+    @prototypes = Prototype.order("prototypes.likes_count DESC").eager_load(:user, :prototype_images)
     @status = 'popular'
+    render 'prototypes/index'
   end
 end
