616dd9b272e75719fc389f8720ce713b7a069b12
yogoken <yogo.k@husky.neu.edu>
2016-08-08 19:27:45 +0900

    Update tags/show and show action with eager_load on tags controller

--- app/controllers/prototypes/tags_controller.rb
+++ app/controllers/prototypes/tags_controller.rb
@@ -4,5 +4,11 @@ class Prototypes::TagsController < ApplicationController
   end
 
   def show
+    @tag = ActsAsTaggableOn::Tag.find(params[:id])
+    @prototypes = Prototype.tagged_with(@tag).eager_load(:user, :prototype_images)
   end
 end
+
+# 確認3 eager_loadとincludeがなかなか使いこなせない。大きな違いは他のテーブルとの結合を必ずしたいかそうではないかなのですか？
+# 結果的に少ない方が良さそうなので両方試してsplの発行回数、合計時間を比べます。
+# 検証結果 eager_load (1.3 ms), includes (1.7 ms) よって、今回はeager_loadを使用
