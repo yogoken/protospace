189f1a22628e0dcef7c5b0de1926af0d81c06d3c
yogoken <yogo.k@husky.neu.edu>
2016-08-12 21:16:36 +0900

    Add multi-environment yaml settings to carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,19 +2,19 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'us-east-1'
+    aws_access_key_id: Settings.s3.access_key,
+    aws_secret_access_key: Settings.s3.secret_access_key,
+    region: Settings.s3.region
   }
 
     case Rails.env
     when 'development'
-        config.fog_directory  = 'protospace-uploading-images'
-        config.asset_host = 'https://s3.amazonaws.com/protospace-uploading-images'
+        config.fog_directory  = Settings.s3.fog_directory
+        config.asset_host = Settings.s3.asset_host
         config.storage = :fog
     when 'production'
-        config.fog_directory  = 'protospace-uploading-images'
-        config.asset_host = 'https://s3.amazonaws.com/protospace-uploading-images'
+        config.fog_directory  = Settings.s3.fog_directory
+        config.asset_host = Settings.s3.asset_host
         config.storage = :fog
     when 'test'
         config.storage = :file
