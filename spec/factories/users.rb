99235afc01686c8820cf3b01ae7368f83fc08982
yogoken <yogo.k@husky.neu.edu>
2016-08-14 10:35:01 +0900

    Complete User model rspec test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     password = Faker::Internet.password(8)
-    name { Faker::Internet.user_name }
+    username { Faker::Internet.user_name }
     email { Faker::Internet.email }
     password password
     password_confirmation password
