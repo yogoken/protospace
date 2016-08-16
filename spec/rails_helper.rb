15160f8eee5cd074d8fa7ce2d8a8441718974539
yogoken <yogo.k@husky.neu.edu>
2016-08-17 00:45:27 +0900

    Complete posing prototype feature test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,8 @@ require 'devise'
 require 'support/controller_macros'
 
 Capybara.javascript_driver = :poltergeist
-
+# [重要]RSpec3ではデフォルトでspec/support/を自動的に読み込まない設定になっていたので、デフォルトでコメントアウトされているものを外す必要がある。これにより、spec/supportディレクトリ内のファイルも読み込まれるようになる
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
